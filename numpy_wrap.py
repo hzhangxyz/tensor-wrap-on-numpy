d57a93266c5e309bbb5a8cae224364d0deb1b250
Hao Zhang <zh970205@outlook.com>
2018-09-23 15:09:13 +0800

    contract

--- numpy_wrap.py
+++ numpy_wrap.py
@@ -7,9 +7,19 @@ class tensor(ndarray):
         assert len(set(obj.legs)) == len(obj.legs), "repeated legs name"
         return obj
 
+    def __repr__(self):
+        return f'{super().__repr__()}\nlegs({self.legs})'
+
     def __array_finalize__(self, obj):
-        if obj is None: return
-        self.legs = getattr(obj, 'legs', None)
+        if obj is None:
+            return
+        legs = getattr(obj, 'legs', None)
+        if legs is None:
+            self.legs = legs
+            return
+        print("legs",legs)
+        self.legs = [str(legs[i]) for i in range(self.ndim) ]
+        assert len(set(self.legs)) == len(self.legs), "repeated legs name"
 
     def set_legs(self, legs):
         self.legs = [str(legs[i]) for i in range(self.ndim) ]
@@ -29,3 +39,21 @@ class tensor(ndarray):
         res = self.transpose([self.legs.index(str(i)) for i in legs])
         res.set_legs(legs)
         return res
+
+    def tensor_contract(self, tensor, legs1, legs2,
+            legs_dict1=None, legs_dict2=None):
+        tensor1 = self
+        tensor2 = tensor
+        legs_dict1 = {} if legs_dict1 is None else legs_dict1
+        legs_dict2 = {} if legs_dict2 is None else legs_dict2
+        order1 = [tensor1.legs.index(str(i)) for i in legs1]
+        order2 = [tensor2.legs.index(str(i)) for i in legs2]
+        legs = [j if j not in legs_dict1 else legs_dict1[j] \
+                for j in tensor1.legs if j not in legs1] +\
+                [j if j not in legs_dict2 else legs_dict2[j] \
+                for j in tensor2.legs if j not in legs2]
+        res = tensordot(tensor1, tensor2, [order1, order2])
+        return self.__class__(res, legs=legs)
+
+    def tensor_svd():
+        pass
