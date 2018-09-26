55e9dc316182b8f477369dbcb3b0ce4d1a2fa40a
Hao Zhang <zh970205@outlook.com>
2018-09-27 05:35:28 +0800

    命令行接口

--- main.py
+++ main.py
@@ -4,24 +4,32 @@ import numpy_wrap as np
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-N', '--size-n', dest='n', required=True, type=int)
-parser.add_argument('-M', '--size-m', dest='m', required=True, type=int)
-parser.add_argument('-d', '--dim', dest='D', required=True, type=int)
-parser.add_argument('-D', '--dim-cut', dest='D_c', required=True, type=int)
-parser.add_argument('-t', '--scan-time', dest='scan_time', required=True, type=int)
-parser.add_argument('-s', '--step-size', dest='step_size', required=True, type=float)
-parser.add_argument('-m', '--markov-chain-length', dest='markov_chain_length', required=True, type=int)
-parser.add_argument('-f', '--load-from', dest='load_from')
-parser.add_argument('-p', '--save-prefix', dest='save_prefix', default='run')
-parser.add_argument('-c', '--continue', dest='continued', default=False, action="store_true")
+parser.add_argument('-S', '--simple-update', dest='update', default=False, action="store_true", help="run simple update instead of gradient descent")
+parser.add_argument('-c', '--continue', dest='continued', default=False, action="store_true", help="continue run, conflict with -f")
+parser.add_argument('-N', '--size-n', dest='n', required=True, type=int, help="system size n")
+parser.add_argument('-M', '--size-m', dest='m', required=True, type=int, help="system size m")
+parser.add_argument('-d', '--dim', dest='D', required=True, type=int, help="bond dimension")
+parser.add_argument('-D', '--dim-cut', dest='D_c', required=True, type=int, help="dimension cut in MPO")
+parser.add_argument('-s', '--scan-time', dest='scan_time', required=True, type=int, help="scan time in MPO")
+parser.add_argument('-l', '--step-size', dest='step_size', required=True, type=float, help="step size in SU or GM")
+parser.add_argument('-m', '--markov', dest='markov', required=True, type=int, help="markov chain length")
+parser.add_argument('-f', '--load-from', dest='load_from', help="load from file")
+parser.add_argument('-p', '--save-prefix', dest='save_prefix', default='run', help="prefix for saving data")
+parser.add_argument('-e', '--step-print', dest='print_step', type=int, help="how many step print once in SU")
 args = parser.parse_args()
+
+if args.continued and args.load_from != None:
+    exit("conflict between --continue and --load-from")
+
+if args.update and args.print_step == None:
+    exit("--step-print needed when simple update")
 
 if args.continued and not args.load_from:
     args.load_from = f"{args.save_prefix}/last/last.npz"
 
-lattice = SL(args.n, args.m, args.D, args.D_c, args.scan_time, args.step_size, args.markov_chain_length, args.load_from, args.save_prefix)
+lattice = SL(args.n, args.m, args.D, args.D_c, args.scan_time, args.step_size, args.markov, args.load_from, args.save_prefix)
 
-#if not args.continued:
-#lattice.itebd(False)
-
-lattice.grad_descent()
+if args.update:
+    lattice.itebd()
+else:
+    lattice.grad_descent()
