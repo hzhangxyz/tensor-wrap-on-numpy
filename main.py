f9f6335b3b43c82a223971439a8f80dccaa6c7f5
Hao Zhang <zh970205@outlook.com>
2018-10-01 03:40:28 +0800

    再改一下...

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import numpy as np
 import tensorflow as tf
-from tensor import Node
-from spin_state import SpinState
-from tensor_square import SquareLattice
 
-
+from . import *
---
+++ tnsp/__init__.py
@@ -0,0 +1,5 @@
+from tensor_node import Node
+from spin_state import SpinState
+from square_lattice import SquareLattice
+
+__all__ = ['Node', 'SpinState', 'SquareLattice']
