5e3b06eef2dfbc3e59bc6f1f7c9d5f53c28bd2c9
Hao Zhang <hao.zhang.xyz@gmail.com>
2018-11-09 19:26:49 +0800

    删掉gm里面留着个素、

--- runGM.py
+++ runGM.py
@@ -6,7 +6,6 @@ import argparse
 import tensorflow as tf
 from tnsp import SquareLattice
 parser = argparse.ArgumentParser()
-#parser.add_argument('-S', '--simple-update', dest='update', default=False, action="store_true", help="run simple update instead of gradient descent")
 parser.add_argument('-c', '--continue', dest='continued', default=False, action="store_true", help="continue run, conflict with -f")
 parser.add_argument('-N', '--size-n', dest='n', required=True, type=int, help="system size n")
 parser.add_argument('-M', '--size-m', dest='m', required=True, type=int, help="system size m")
@@ -17,18 +16,10 @@ parser.add_argument('-l', '--step-size', dest='step_size', required=True, type=f
 parser.add_argument('-m', '--markov', dest='markov', required=True, type=int, help="markov chain length")
 parser.add_argument('-f', '--load-from', dest='load_from', help="load from file")
 parser.add_argument('-p', '--save-prefix', dest='save_prefix', default='run', help="prefix for saving data")
-#parser.add_argument('-e', '--step-print', dest='step_print', type=int, help="how many step print once in SU")
-#parser.add_argument('-a', '--accurate', dest='accurate', default=False, action="store_true", help="calculate accurate energy rather than markov")
 args = parser.parse_args()
 
 if args.continued and args.load_from != None:
     exit("conflict between --continue and --load-from")
-
-# if args.update and args.step_print == None:
-#    exit("--step-print needed when simple update")
-
-# if not args.update and args.accurate:
-#    exit("--accurate only work for simple update")
 
 if args.continued and not args.load_from:
     args.load_from = f"{args.save_prefix}/last/last.npz"
