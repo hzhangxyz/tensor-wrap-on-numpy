353fc7b2f4270312509a0769f6b5fca684aec518
Hao Zhang <hao.zhang.xyz@gmail.com>
2018-11-10 00:26:17 +0800

    用pid做种子

--- tnsp/op/next_hop.cc
+++ tnsp/op/next_hop.cc
@@ -4,6 +4,7 @@
 #include <ctime>
 #include <iostream>
 #include <mutex>
+#include <unistd.h>
 
 using namespace tensorflow;
 
@@ -12,58 +13,58 @@ REGISTER_OP("NextHop")
 .Output("stay_step: int32")
 .Output("next_index: int32")
 .SetShapeFn([](::tensorflow::shape_inference::InferenceContext* c) {
-        c->set_output(0, c->Scalar());
-        c->set_output(1, c->Scalar());
-        return Status::OK();
-});
+              c->set_output(0, c->Scalar());
+              c->set_output(1, c->Scalar());
+              return Status::OK();
+            });
 
 std::once_flag random_seed_flag;
 
 class NextHopOp : public OpKernel {
-    public:
-        explicit NextHopOp(OpKernelConstruction* context) : OpKernel(context) {}
+public:
+  explicit NextHopOp(OpKernelConstruction* context) : OpKernel(context) {}
 
-        void Compute(OpKernelContext* context) override {
-            const Tensor& possibility_handle = context->input(0);
-            auto total_n = possibility_handle.dim_size(0);
-            auto possibility = possibility_handle.flat<float>();
+  void Compute(OpKernelContext* context) override {
+    const Tensor& possibility_handle = context->input(0);
+    auto total_n = possibility_handle.dim_size(0);
+    auto possibility = possibility_handle.flat<float>();
 
-            int flag[total_n];
-            int eff_n = 0;
-            for(int i = 0; i < total_n; i++){
-                if(possibility(i)>=0){
-                    flag[eff_n++] = i;
-                }
-            }
+    int flag[total_n];
+    int eff_n = 0;
+    for(int i = 0; i < total_n; i++){
+      if(possibility(i)>=0){
+        flag[eff_n++] = i;
+      }
+    }
 
-            int stay_step = 0;
-            int next_index = 0;
-            std::call_once(random_seed_flag, []{
-                    std::srand(std::time(0));
-                    });
-            //std::srand((int)(possibility(flag[0])*65535));
-            for(float hop_p=0, rand_n=1;hop_p<rand_n;++stay_step){
-                next_index = std::rand()%eff_n;
-                hop_p = possibility(flag[next_index]);
-                rand_n= ((float) std::rand()) / (float) RAND_MAX;
-            }
+    int stay_step = 0;
+    int next_index = 0;
+    std::call_once(random_seed_flag, []{
+                                       std::srand(getpid());
+                                     });
+    //std::srand((int)(possibility(flag[0])*65535));
+    for(float hop_p=0, rand_n=1;hop_p<rand_n;++stay_step){
+      next_index = std::rand()%eff_n;
+      hop_p = possibility(flag[next_index]);
+      rand_n= ((float) std::rand()) / (float) RAND_MAX;
+    }
 
-            Tensor* res = NULL;
-            TensorShape shape;
-            int dims[] = {};
-            TensorShapeUtils::MakeShape(dims, 0, &shape);
+    Tensor* res = NULL;
+    TensorShape shape;
+    int dims[] = {};
+    TensorShapeUtils::MakeShape(dims, 0, &shape);
 
-            OP_REQUIRES_OK(context, context->allocate_output(0, shape,
-                        &res));
-            auto out1 = res->flat<int32>();
-            out1(0) = stay_step;
+    OP_REQUIRES_OK(context, context->allocate_output(0, shape,
+                                                     &res));
+    auto out1 = res->flat<int32>();
+    out1(0) = stay_step;
 
-            OP_REQUIRES_OK(context, context->allocate_output(1, shape,
-                        &res));
-            auto out2 = res->flat<int32>();
-            out2(0) = flag[next_index];
+    OP_REQUIRES_OK(context, context->allocate_output(1, shape,
+                                                     &res));
+    auto out2 = res->flat<int32>();
+    out2(0) = flag[next_index];
 
-        }
+  }
 };
 
 REGISTER_KERNEL_BUILDER(Name("NextHop").Device(DEVICE_CPU), NextHopOp);
