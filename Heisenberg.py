ab6f8f0a49fa93883bb56ef21bdd8632b3d68aac
Hao Zhang <hao.zhang.xyz@gmail.com>
2018-11-10 17:13:40 +0800

    移动到另外一个文件

---
+++ Heisenberg.py
@@ -0,0 +1,42 @@
+# Kitaev Model
+
+import numpy as np
+
+def get_lattice_node_leg(i, j, n, m):
+    legs = 'lrud'
+    if i == 0:
+        legs = legs.replace('u', '')
+    if i == n-1:
+        legs = legs.replace('d', '')
+    if j == 0:
+        legs = legs.replace('l', '')
+    if j == m-1:
+        legs = legs.replace('r', '')
+    #if (i+j) % 2 == 0:
+    #    legs = legs.replace('l', '')
+    #else:
+    #    legs = legs.replace('r', '')
+    return legs
+
+Sz = np.array([[ 1, 0, 0, 0],
+               [ 0,-1, 0, 0],
+               [ 0, 0,-1, 0],
+               [ 0, 0, 0, 1]])/4.
+Sx = np.array([[ 0, 0, 0, 1],
+               [ 0, 0, 1, 0],
+               [ 0, 1, 0, 0],
+               [ 1, 0, 0, 0]])/4.
+Sy = np.array([[ 0, 0, 0,-1],
+               [ 0, 0, 1, 0],
+               [ 0, 1, 0, 0],
+               [-1, 0, 0, 0]])/4.
+Zero = np.array([[ 0, 0, 0, 0],
+                 [ 0, 0, 0, 0],
+                 [ 0, 0, 0, 0],
+                 [ 0, 0, 0, 0]])/4.
+
+def Hop(a,b,c,d):
+    return True
+
+def Hamiltonian(a,b,c,d):
+    return Sx+Sy+Sz
