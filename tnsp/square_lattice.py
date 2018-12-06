51400dcc65ad7f0963256cd5b5533c82c31feb3c
Hao Zhang <hao.zhang.xyz@gmail.com>
2018-12-06 22:23:36 +0800

    删掉一个barrier

--- tnsp/square_lattice.py
+++ tnsp/square_lattice.py
@@ -39,13 +39,12 @@ class SquareLattice():
             self.lattice = [[self.__create_node(i, j) for j in range(m)] for i in range(n)]
         else:
             self.prepare = np.load(load_from)
-            mpi_comm.Barrier()
             if mpi_rank==0:
-                print(f'{load_from} loaded')
                 self.lattice = [[self.__check_shape(self.prepare[f'node_{i}_{j}'], i, j) for j in range(m)] for i in range(n)]
             else:
                 self.lattice = None
             self.lattice = mpi_comm.bcast(self.lattice, root=0)
+            print(f'{load_from} loaded')
         # 载入lattice信息
 
         self.D_c = D_c
