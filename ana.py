2749672a9c6574ba13bb08237ac2916b56a86fa3
Hao Zhang <hao.zhang.xyz@gmail.com>
2018-12-11 01:31:50 +0800

    更改ana.py

--- ana.py
+++ ana.py
@@ -4,23 +4,25 @@ from matplotlib import pyplot as plt
 
 def getAll(base):
     thisData = np.loadtxt(f'{base}/GM.log')
+    label = " " .join(np.loadtxt(f"{base}/parameter",dtype=str))
     if os.path.exists(f'{base}/load_from'):
         loadFrom = os.path.realpath(f'{base}/load_from')
         nextBase, offSetFile = os.path.split(loadFrom)
         offSet = int(offSetFile[3:-4])
         result = getAll(nextBase)
-        result.append((offSet+result[-1][0]+1, thisData))
+        result.append((offSet+result[-1][0]+1, thisData, label))
         return result
-    return [(0, thisData)]
+    return [(0, thisData, label)]
 
 def getPoint(base):
     allData = getAll(base)
-    data = [i[1].T+[[i[0]],[0]] for i in allData]
+    data = [(i[1].T[0]+i[0], i[1].T[1], i[2]) for i in allData]
     return data
 
 def plotIt(base, acc=-0.16580050716890718,lim=(1.001,0.975),size=1):
     data = getPoint(base)
     fig, ax = plt.subplots(figsize=(18, 12))
-    _ = [ax.plot(*i,'.',markersize=size) for i in data]
+    [ax.plot(i[0], i[1] ,'.',markersize=size, label=i[2]) for i in data]
+    ax.legend(loc='upper right', markerscale=10/size)
     ax.axhline(y=acc)
-    _ = ax.set_ylim(acc*lim[0],acc*lim[1])
+    ax.set_ylim(acc*lim[0],acc*lim[1])
